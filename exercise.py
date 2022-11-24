75ce05d7794b7ffaa34fb702b4fc1775e01a61e5
Mattia Monga <mattia.monga@unimi.it>
2022-11-24 20:18:40 +0100

    ay 22/23

--- exercise.py
+++ exercise.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt                 # type: ignore
+from matplotlib.animation import FuncAnimation  # type: ignore
+from IPython.display import HTML                # type: ignore
 
 
 def zero_border(grid: np.ndarray) -> np.ndarray:
@@ -14,6 +17,21 @@ def zero_border(grid: np.ndarray) -> np.ndarray:
     ris[[0, -1], :] = 0
     ris[:, [0, -1]] = 0
     return ris
+
+
+def has_zero_border(grid: np.ndarray) -> bool:
+    """True iff the grid has a zero border.
+
+    >>> g = np.zeros((10, 10))
+    >>> has_zero_border(g)
+    True
+
+    >>> g = np.ones((10, 10))
+    >>> has_zero_border(g)
+    False
+
+    """
+    return (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
 
 
 def neighbour_count(grid: np.ndarray, x: int, y: int) -> int:
@@ -36,7 +54,7 @@ def neighbour_count(grid: np.ndarray, x: int, y: int) -> int:
     2
 
     """
-    assert (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
+    assert has_zero_border(grid)
     ris = 0
     for dx in (-1, 0, 1):
         for dy in (-1, 0, 1):
@@ -63,7 +81,7 @@ def neighbour_matrix(grid: np.ndarray) -> np.ndarray:
            [0, 0, 0, 0, 0, 0]])
 
     """
-    assert (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
+    assert has_zero_border(grid)
     ris = np.zeros_like(grid)
     for i in range(1, grid.shape[0]-1):
         for j in range(1, grid.shape[1]-1):
@@ -74,6 +92,8 @@ def neighbour_matrix(grid: np.ndarray) -> np.ndarray:
 def neighbour_matrix_v(grid: np.ndarray) -> np.ndarray:
     """Compute the matrix of neighbours without using loops.
 
+    Hint: what does an expression like grid[2:, 1:-1] denote?
+
     >>> rng = np.random.default_rng(seed=42)
     >>> g = rng.integers(0, 2, size=(10,20))
     >>> g = zero_border(g)
@@ -81,7 +101,7 @@ def neighbour_matrix_v(grid: np.ndarray) -> np.ndarray:
     True
 
     """
-    assert (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
+    assert has_zero_border(grid)
     ris = np.zeros_like(grid)
     ris[1:-1, 1:-1] = (grid[2:, 1:-1] + grid[2:, 2:] + grid[1:-1, 2:] +
                        grid[:-2, 2:] + grid[:-2, 1:-1] + grid[:-2, :-2] +
@@ -107,7 +127,7 @@ def new_generation(grid: np.ndarray) -> np.ndarray:
            [0, 0, 0, 0, 0, 0]])
 
     """
-    assert (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
+    assert has_zero_border(grid)
     ris = grid.copy()
     neighs = neighbour_matrix(grid)
     for i in range(1, grid.shape[0]-1):
@@ -124,13 +144,15 @@ def new_generation(grid: np.ndarray) -> np.ndarray:
 def new_generation_v(grid: np.ndarray) -> np.ndarray:
     """Compute new generation without using loops.
 
+    Hint: what is the result of the expression grid == 0?
+
     >>> rng = np.random.default_rng(seed=42)
     >>> g = rng.integers(0, 2, size=(10,20))
     >>> g = zero_border(g)
     >>> (new_generation_v(g) == new_generation(g)).all()
     True
     """
-    assert (grid[[0, -1], :] == 0).all() and (grid[:, [0, -1]] == 0).all()
+    assert has_zero_border(grid)
     ris = grid.copy()
     neighs = neighbour_matrix_v(grid)
 
@@ -143,11 +165,10 @@ def new_generation_v(grid: np.ndarray) -> np.ndarray:
     return ris
 
 
-import matplotlib.pyplot as plt  # type: ignore
-
-SIZE = 50
+SIZE = 1000                  # DONE
+ng_maker = new_generation_v  # DONE
 rng = np.random.default_rng(seed=42)
-G = rng.integers(0, 2, size=(SIZE,2*SIZE))
+G = rng.integers(0, 2, size=(SIZE, 2*SIZE))
 G = zero_border(G)
 
 fig, ax = plt.subplots()
@@ -156,31 +177,27 @@ ax.set_xticks([])
 _ = ax.set_yticks([])
 
 fig, ax = plt.subplots()
-im = ax.imshow(new_generation(G), cmap=plt.cm.gray_r, vmin=0, vmax=1)
+im = ax.imshow(ng_maker(G), cmap=plt.cm.gray_r, vmin=0, vmax=1)
 ax.set_xticks([])
 _ = ax.set_yticks([])
 
-from matplotlib.animation import FuncAnimation  # type: ignore
-
 
 class Life:
-    def __init__(self, grid: np.ndarray):
+    def __init__(self, grid: np.ndarray) -> None:
         self.grid = grid
         self.fig, ax = plt.subplots()
         self.im = ax.imshow(self.grid, cmap=plt.cm.gray_r, vmin=0, vmax=1)
         ax.set_xticks([])
         ax.set_yticks([])
 
-    def update(self, frame: int):
-        self.grid = new_generation_v(self.grid)
+    def update(self, frame: int) -> None:
+        self.grid = ng_maker(self.grid)
         self.im.set_data(self.grid)
 
-    def animate(self, frames: int):
+    def animate(self, frames: int) -> None:
         self.animation = FuncAnimation(self.fig, self.update, frames=frames)
 
 
 life = Life(G)
 life.animate(100)
-
-from IPython.display import HTML  # type: ignore
 HTML(life.animation.to_jshtml())
